2c070863a67901714220c3caadef4ae948ec1e48
Will Spaur <mitchellspaur@gmail.com>
2021-05-13 16:20:22 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+
+A library app that takes in user input for books including the titles, author, page number, and if read or not.
+
+This app both allows for form input from the user, logs these books, then displays them back on the page. There's a toggle for read/no read on each book. 
+
+
+
+
+*/
